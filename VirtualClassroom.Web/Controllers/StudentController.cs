33d783305720a838d0fda7e74f96fa71033642da
shrutihansaliya <shrutihansaliya2005@gmail.com>
2026-04-05 11:29:58 +0530

    Initial commit - Virtual Classroom project

---
+++ VirtualClassroom.Web/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualClassroom.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
