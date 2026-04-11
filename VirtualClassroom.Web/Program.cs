7e7affe0dc7bbb2df24d82113f57b435351c7303
Vamjahetvi <hetuvamja22@gmail.com>
2026-04-11 10:56:27 +0530

    forgot Password done

--- VirtualClassroom.Web/Program.cs
+++ VirtualClassroom.Web/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDataProtection();
+builder.Services.AddScoped<EmailService>();
+
 //session
 builder.Services.AddSession(options =>
 {
